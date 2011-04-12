534167e5840e44fccf593da95e86a6d09935569f
isaak <lintner.lim@googlemail.com>
2011-04-12 21:44:40 +0200

    back to basics

--- OpenGLMain.cpp
+++ OpenGLMain.cpp
@@ -18,6 +18,8 @@
 using namespace std;
 
 // menu params --------------------------
+#define WIDTH 800
+#define HEIGHT 600
 unsigned int frame = 0;
 int timebase = 0;
 int currenttime = 0;
@@ -43,7 +45,9 @@ enum {
    R_C
 };
 
-float gauss[] = {0, 0, 1, 1, 0};
+float gauss[] = {400, 300, 1, 1, 0};
+GLfloat mu[] = {gauss[MU_X], gauss[MU_Y]};
+GLfloat cov[] = {gauss[S_X], gauss[R_C], gauss[R_C], gauss[S_Y]};
 GLint splattingShader = 0;
 GLint gaussLoc = 0;
 // --------------------------------------
@@ -86,107 +90,13 @@ char* readShaderFromFile(const string& fileName) {
  */
 void initOpenGL(int width, int height) {
    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-   glClearDepth(1.0);
-   glDepthFunc(GL_LESS);
    glEnable(GL_DEPTH_TEST);
-   glShadeModel(GL_SMOOTH);
 
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
-   gluPerspective(45.0f, (GLdouble)width/(GLdouble)height, 0.1f, 100.0f);
+   gluPerspective(45.0f, (GLdouble)width/(GLdouble)height, 1, 1000.0f);
 
    glMatrixMode(GL_MODELVIEW);
-
-   // compile and link the shader program
-   const char* vertexSrc = readShaderFromFile("Splatting.vert");
-   const char* geomSrc = readShaderFromFile("Splatting.geom");
-   const char* fragSrc = readShaderFromFile("Splatting.frag");
-   int isCompiledVS, isCompiledFS, isCompiledGEO, maxLength, isLinked;
-
-   splattingShader = glCreateProgram();
-   // compile the vertex shader
-   int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-   glShaderSource(vertexShader, 1, (const GLchar**)&vertexSrc, 0);
-   glCompileShader(vertexShader);
-   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiledVS);
-   if (!isCompiledVS) {
-      glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
-      char* vertexInfoLog = new char[maxLength];
-
-      glGetShaderInfoLog(vertexShader, maxLength, &maxLength, vertexInfoLog);
-      fprintf(stdout, "vertex shader: compile error: ");
-      fprintf(stdout, vertexInfoLog);
-      delete[] vertexInfoLog;
-      return;
-   }
-
-   // compile the geometry shader
-   int geoShader = glCreateShader(GL_GEOMETRY_SHADER);
-   glShaderSource(geoShader, 1, (const GLchar**)&geomSrc, 0);
-   glCompileShader(geoShader);
-   glGetShaderiv(geoShader, GL_COMPILE_STATUS, &isCompiledGEO);
-   if (!isCompiledGEO) {
-      glGetShaderiv(geoShader, GL_INFO_LOG_LENGTH, &maxLength);
-      char* geoInfoLog = new char[maxLength];
-
-      glGetShaderInfoLog(geoShader, maxLength, &maxLength, geoInfoLog);
-      fprintf(stdout, "geometry shader: compile error: ");
-      fprintf(stdout, geoInfoLog);
-      delete[] geoInfoLog;
-      return;
-   }
-
-   // compile the fragment shader
-   int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-   glShaderSource(fragShader, 1, (const GLchar**)&fragSrc, 0);
-   glCompileShader(fragShader);
-   glGetShaderiv(fragShader, GL_COMPILE_STATUS, &isCompiledFS);
-   if (!isCompiledFS) {
-      glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &maxLength);
-      char* fragInfoLog = new char[maxLength];
-
-      glGetShaderInfoLog(fragShader, maxLength, &maxLength, fragInfoLog);
-      fprintf(stdout, "fragment shader: compile error: ");
-      fprintf(stdout, fragInfoLog);
-      delete[] fragInfoLog;
-      return;
-   }
-
-   // attach and link the shaders to the program
-   glAttachShader(splattingShader, vertexShader);
-   glAttachShader(splattingShader, geoShader);
-   glAttachShader(splattingShader, fragShader);
-
-   glLinkProgram(splattingShader);
-   glGetProgramiv(splattingShader, GL_LINK_STATUS, (int*)&isLinked);
-   if (!isLinked) {
-      glGetProgramiv(splattingShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-      char* splattingInfoLog = new char[maxLength];
-
-      glGetProgramInfoLog(splattingShader, maxLength, &maxLength, splattingInfoLog);
-      fprintf(stdout, "shader: link error: ");
-      fprintf(stdout, splattingInfoLog);
-      delete[] splattingInfoLog;
-      return;
-   }
-
-   muLoc = glGetAttribLocation(splattingShader, "mu");
-   covLoc = glGetAttribLocation(splattingShader, "cov");
-   projectionLoc = glGetUniformLocation(splattingShader, "projectionMatrix");
-   modelViewLoc = glGetUniformLocation(splattingShader, "modelViewMatrix");
-
-   // passes the projection matrix to the shader
-   GLfloat pProj[16];
-   glGetFloatv(GL_PROJECTION_MATRIX, pProj);
-   glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, pProj);
-
-
-
-   // generate buffer objects
-   glGenBuffers(1, &gaussVBO);
-   glBindBuffer(GL_ARRAY_BUFFER, gaussVBO);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(gauss), &gauss, GL_DYNAMIC_DRAW);
 }
 
 /*
@@ -200,7 +110,7 @@ void resizeOpenGLScene(int width, int height) {
 
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
-   gluPerspective(45.0f, (GLdouble)width/(GLdouble)height, 0.1f, 100.0f);
+   gluPerspective(45.0f, (GLdouble)width/(GLdouble)height, 1, 1000.0f);
 
    glMatrixMode(GL_MODELVIEW);
 }
@@ -343,72 +253,11 @@ void drawMenu() {
 void drawOpenGLScene() {
    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
    glLoadIdentity();
-   gluLookAt(  0, 0, 5,
-               0, 0, -1,
-               0, 1, 0);
+   gluLookAt(0, 0, 5,
+             0, 0, -1,
+             0, 1, 0);
 
-   // passes the modelview matrix to the shader
-   GLfloat pModView[16];
-   glGetFloatv(GL_MODELVIEW, pModView);
-   glUniformMatrix4fv(modelViewLoc, 1, GL_FALSE, pModView);
-
-   /*
-   // tempory calculation of bounding box (mu is 0,0)
-   float trace = gauss[S_X] + gauss[S_Y];
-   float det = gauss[S_X] * gauss[S_Y] - pow(gauss[R_C], 2);
-   float eigenVal1 = (trace + sqrt(pow(trace, 2) - 4 * det))/2;
-   float eigenVal2 = (trace - sqrt(pow(trace, 2) - 4 * det))/2;
-
-   // length of each axis of the ellipse
-   float lengthX = sqrt(eigenVal1);
-   float lengthY = sqrt(eigenVal2);
-
-   // orientation of the first eigenvector in relation to the x axis
-   float angleDeg = 0;
-   if (gauss[R_C]) {
-      Vector eigenVec1 = Vector(eigenVal1 - gauss[S_Y], gauss[R_C], 0);
-      //Vector eigenVec2 = Vector(eigenVal2 - s_y, c, 0);
-      float angleRad = acos((eigenVec1 ^ Vector(0, 1, 0))/eigenVec1.length());
-      angleDeg = angleRad * 180/PI;
-   }
-
-   // draw bounding rectangle
-   if (menuStates[WIREFRAME]) {
-      glPushMatrix();
-
-      if (gauss[R_C])
-         glRotatef(angleDeg, 0, 0, 1);
-      glColor3f(1, 0, 0);
-
-      glBegin(GL_LINE_STRIP);
-      glVertex3f(-lengthX, -lengthY, 0);
-      glVertex3f(lengthX, -lengthY, 0);
-      glVertex3f(lengthX, lengthY, 0);
-      glVertex3f(-lengthX, lengthY, 0);
-      glVertex3f(-lengthX, -lengthY, 0);
-      glEnd();
-
-      glPopMatrix();
-   }
-   */
-
-   glUseProgram(splattingShader);
-
-   glBindBuffer(GL_ARRAY_BUFFER, gaussVBO);
-   glEnableVertexAttribArray(muLoc);
-   glEnableVertexAttribArray(covLoc);
-
-   glVertexAttribPointer(muLoc, 2, GL_FLOAT, GL_FALSE, sizeof(gauss), (void*)0);
-   glVertexAttribPointer(covLoc, 3, GL_FLOAT, GL_FALSE, sizeof(gauss), (void*)(sizeof(GL_FLOAT)*2));
-
-   glDrawArrays(GL_POINTS, 0, 1);
-   //GLenum error = glGetError();
-
-   glDisableVertexAttribArray(covLoc);
-   glDisableVertexAttribArray(muLoc);
-   glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-   glUseProgram(0);
+   glutSolidTeapot(1);
 
    drawMenu();
 
@@ -421,7 +270,7 @@ int main(int argc, char **argv) {
    // use double buffer
    glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
 
-   glutInitWindowSize(800, 600);
+   glutInitWindowSize(WIDTH, HEIGHT);
 
    glutInitWindowPosition(0, 0);
 
@@ -443,7 +292,7 @@ int main(int argc, char **argv) {
    }
    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
-   initOpenGL(800, 600);
+   initOpenGL(WIDTH, HEIGHT);
 
    timebase = glutGet(GLUT_ELAPSED_TIME);
 
